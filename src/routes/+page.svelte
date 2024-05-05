1b466fa102a2a85003a2f94d07da893759545c48
Kyle He <panyanhe99@gmail.com>
2024-05-05 11:21:53 -0700

    Add basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // Add your script logic here
+</script>
+
+<main class="container mx-auto max-w-3xl p-4">
+  <h1 class="font-serif text-4xl font-bold text-left mt-8">Kyle He</h1>
+  <p class="font-serif text-base text-left mt-8">I'm currently a student at the USC studying Computer Science. </p>
+    
+</main>
