50865f9510e535d1e9450292b720fe38f34d46af
Kyle He <panyanhe99@gmail.com>
2024-10-26 14:03:18 -0700

    Add links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,51 @@
 <script>
 </script>
 
+<head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+  />
+</head>
+
 <main class="container mx-auto max-w-3xl p-4">
   <h1 class="font-serif text-4xl font-bold text-left mt-8">Kyle He</h1>
-  <p class="font-serif text-base text-left mt-8">I'm currently a student at the USC studying Computer Science. </p>
-    
+  <p class="font-serif text-base text-left mt-8">
+    I'm currently a student at the USC studying Computer Science. 
+  </p>
+
+  <section class="mt-8">
+    <h2 class="font-serif text-2xl font-semibold text-left">Find me here</h2>
+    <ul class="font-serif text-base text-left mt-4">
+      <li class="mt-2 flex items-center">
+        <i class="fab fa-github text-blue-700 mr-2"></i>
+        <a
+          href="https://github.com/kyle-he"
+          class="text-blue-700 hover:underline transition duration-300"
+          >github (kyle-he)</a
+        >
+      </li>
+      <li class="mt-2 flex items-center">
+        <i class="fab fa-linkedin text-blue-700 mr-2"></i>
+        <a
+          href="https://linkedin.com/in/kyle-he"
+          class="text-blue-700 hover:underline transition duration-300"
+          >linkedin (kyle-he)</a
+        >
+      </li>
+    </ul>
+  </section>
+
+  <section class="mt-8">
+    <h2 class="font-serif text-2xl font-semibold text-left">Contact</h2>
+    <p class="font-serif text-base text-left mt-4">
+      Email:
+      <a
+        href="mailto:kphe@usc.edu?body=Hi%20Kyle%2C%0A"
+        class="text-blue-700 hover:underline transition duration-300"
+      >
+        kphe@usc.edu
+      </a>
+    </p>
+  </section>
 </main>
